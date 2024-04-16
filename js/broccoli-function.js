da27140239e79bb784674a407833fa3c86e75b80
Canada-Broccoli <chen0969@algonquinlive.com>
2024-04-16 19:57:05 -0400

    fix whole page

---
+++ js/broccoli-function.js
@@ -0,0 +1,113 @@
+ //活動維護輪播
+ $('.owl-carousel-one').owlCarousel({
+    loop: true, //循環
+    margin: 10, //與右邊圖片的距離
+    nav: true, //導航
+    dots: true,
+    mouseDrag: false, // 滑鼠拖曳
+    touchDrag: false, // 觸控拖曳
+    navText: ['<', '>'],
+    autoplay: false, //自動
+    startPosition: 'URLHash',
+    //rwd
+    responsive: {
+        0: {
+            items: 1 //出現幾個
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+//最新課程
+$('.owl-carousel-new').owlCarousel({
+    items: 1,
+    loop: true, //循環
+    margin: 10, //與右邊圖片的距離  
+    dots: true, //切換按鈕
+    //rwd
+    responsive: {
+        0: {
+            items: 1 //出現幾個
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+//熱門課程輪播
+$('.owl-carousel-best').owlCarousel({
+    items: 3,
+    loop: true, //循環
+    margin: 50, //與右邊圖片的距離
+    nav: true, //導航文字
+    dots: true,
+    autoplay: true, //自動
+
+    //rwd
+    responsive: {
+        0: {
+            items: 1 //出現幾個
+        },
+        400: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+//理周推薦
+$('.owl-carousel-recommend').owlCarousel({
+    items: 3,
+    loop: true, //循環
+    margin: 50, //與右邊圖片的距離
+    nav: false, //導航文字
+    dots: false,
+    autoplay: true, //自動
+
+    //rwd
+    responsive: {
+        0: {
+            items: 1 //出現幾個
+        },
+        400: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+//影音
+$('.owl-carousel-video').owlCarousel({
+    items: 1,
+    merge: true,
+    loop: true,
+    margin: 10,
+    video: true,
+    lazyLoad: true,
+    center: true,
+
+    //rwd
+    responsive: {
+        0: {
+            items: 1 //出現幾個
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
