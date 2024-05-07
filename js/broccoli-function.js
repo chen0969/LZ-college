6c8ec378fdffa737cda127f3d31bcbb05c81dac3
Canada-Broccoli <chen0969@algonquinlive.com>
2024-05-07 20:24:34 +0800

    check rwd

--- js/broccoli-function.js
+++ js/broccoli-function.js
@@ -1,29 +1,35 @@
-// slick slider
-$(document).ready(function(){
-    setWidth();
-    setTimeout(runOwl, 100);
-    setTimeout(runSlider, 110);
-  });
+$(document).ready(function () {
+    function isMobile() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
 
+    if (isMobile()) {
+        console.log("This is a mobile device.");
+        setWidth();
+        setTimeout(mobileOwl, 100);
+        setTimeout(runSlider, 110);
+    } else {
+        console.log("This is not a mobile device.");
+        setWidth();
+        setTimeout(runOwl, 100);
+        setTimeout(runSlider, 110);
+    }
+});
 
 function setWidth() {
     // Check if the device is not a mobile device
-    if (window.innerWidth > 768) {
-        // section width
-        $(document).ready(function(){
-            let sectionW = $(".carousel-inner").width();
-            $(".news_class").each(function(){
-                $(this).width(sectionW);
-            });
-            $(".callAction").width(sectionW);
-            $(".contact > div").width(sectionW);
-            $(".butmBanner").width(sectionW);
-            $("ul.nav.nav-tabs").width(sectionW);
-        });
-    }
+    // section width
+    let sectionW = $(".carousel-inner").width();
+    $(".news_class").each(function () {
+        $(this).width(sectionW);
+    });
+    $(".callAction").width(sectionW);
+    $(".contact > div").width(sectionW);
+    $(".butmBanner").width(sectionW);
+    $("ul.nav.nav-tabs").width(sectionW);
 }
 
-function runSlider(){
+function runSlider() {
     $(".broccoliSlider .cards.slider").slick({
         dots: true,
         infinite: true,
@@ -33,158 +39,208 @@ function runSlider(){
         vertical: true,
         draggable: true,
         verticalSwiping: true,
-        pauseOnHover:false,
+        pauseOnHover: false,
         arrows: false,
         // rwd
-        responsive: [
-          {
-            breakpoint: 750,
+        responsive: [{
+            breakpoint: 800,
             settings: {
-              dots: false,
-              slidesToShow: 1
+                dots: false,
+                slidesToShow: 1,
+
             }
-          }]
-      });
-  
-      $(".verticalSlide.slider").slick({
-          dots: true,
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          vertical: true,
-          draggable: true,
-          verticalSwiping: true,
-          pauseOnHover: false,
-          arrows: false,
-  // rwd
-          responsive: [
-              {
-                breakpoint: 750,
+        }]
+    });
+
+    $(".verticalSlide.slider").slick({
+            dots: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            vertical: true,
+            draggable: true,
+            verticalSwiping: true,
+            pauseOnHover: false,
+            arrows: false,
+            // rwd
+            responsive: [{
+                breakpoint: 800,
                 settings: {
-                  dots: false
+                    dots: false,
+                    slidesToShow: 1,
+
                 }
-              }]
+            }]
         },
-        
-      );
+
+    );
 }
 
-function runOwl(){
-//活動維護輪播
-$('.owl-carousel-one').owlCarousel({
-    loop: true, //循環
-    margin: 10, //與右邊圖片的距離
-    nav: true, //導航
-    dots: true,
-    mouseDrag: false, // 滑鼠拖曳
-    touchDrag: false, // 觸控拖曳
-    navText: ['<', '>'],
-    autoplay: true, //自動
-    startPosition: 'URLHash',
-    //rwd
-    responsive: {
-        0: {
-            items: 1 //出現幾個
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-});
-//最新課程
-$('.owl-carousel-new').owlCarousel({
-    items: 1,
-    loop: true, //循環
-    margin: 10, //與右邊圖片的距離  
-    dots: true, //切換按鈕
-    //rwd
-    responsive: {
-        0: {
-            items: 1 //出現幾個
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-});
+function runOwl() {
+    $('.owl-carousel-one').owlCarousel({
+        loop: true, //循環
+        margin: 10, //與右邊圖片的距離
+        nav: true, //導航
+        dots: true,
+        mouseDrag: false, // 滑鼠拖曳
+        touchDrag: false, // 觸控拖曳
+        navText: ['<', '>'],
+        autoplay: true, //自動
+        startPosition: 'URLHash',
+        //rwd
+        // responsive: {
+        //     0: {
+        //         items: 1 //出現幾個
+        //     },
+        //     600: {
+        //         items: 1
+        //     },
+        //     1000: {
+        //         items: 1
+        //     }
+        // }
+    });
+    //最新課程
+    $('.owl-carousel-new').owlCarousel({
+        items: 1,
+        loop: true, //循環
+        margin: 10, //與右邊圖片的距離  
+        dots: true, //切換按鈕
+        //rwd
+        // responsive: {
+        //     0: {
+        //         items: 1 //出現幾個
+        //     },
+        //     600: {
+        //         items: 1
+        //     },
+        //     1000: {
+        //         items: 1
+        //     }
+        // }
+    });
 
-//熱門課程輪播
-$('.owl-carousel-best').owlCarousel({
-    items: 3,
-    loop: true, //循環
-    margin: 50, //與右邊圖片的距離
-    nav: true, //導航文字
-    dots: true,
-    autoplay: false, //自動
+    //熱門課程輪播
+    $('.owl-carousel-best').owlCarousel({
+        items: 3,
+        loop: true, //循環
+        margin: 50, //與右邊圖片的距離
+        nav: true, //導航文字
+        dots: true,
+        autoplay: false, //自動
 
-    //rwd
-    responsive: {
-        0: {
-            items: 1 //出現幾個
-        },
-        400: {
-            items: 1
-        },
-        1000: {
-            items: 3
-        }
-    }
-});
+        //rwd
+        // responsive: {
+        //     0: {
+        //         items: 1 //出現幾個
+        //     },
+        //     400: {
+        //         items: 1
+        //     },
+        //     1000: {
+        //         items: 3
+        //     }
+        // }
+    });
 
-//理周推薦
-$('.owl-carousel-recommend').owlCarousel({
-    items: 3,
-    loop: true, //循環
-    margin: 50, //與右邊圖片的距離
-    nav: false, //導航文字
-    dots: false,
-    autoplay: true, //自動
+    //理周推薦
+    $('.owl-carousel-recommend').owlCarousel({
+        items: 3,
+        loop: true, //循環
+        margin: 50, //與右邊圖片的距離
+        nav: false, //導航文字
+        dots: false,
+        autoplay: true, //自動
 
-    //rwd
-    responsive: {
-        0: {
-            items: 1 //出現幾個
-        },
-        400: {
-            items: 1
-        },
-        1000: {
-            items: 4
-        }
-    }
-});
+        //rwd
+        // responsive: {
+        //     0: {
+        //         items: 1 //出現幾個
+        //     },
+        //     400: {
+        //         items: 1
+        //     },
+        //     1000: {
+        //         items: 4
+        //     }
+        // }
+    });
 
-//影音
-$('.owl-carousel-video').owlCarousel({
-    items: 1,
-    merge: true,
-    loop: true,
-    margin: 10,
-    video: true,
-    lazyLoad: true,
-    center: true,
+    //影音
+    $('.owl-carousel-video').owlCarousel({
+        items: 1,
+        merge: true,
+        loop: true,
+        margin: 10,
+        video: true,
+        lazyLoad: true,
+        center: true,
 
-    //rwd
-    responsive: {
-        0: {
-            items: 1 //出現幾個
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-});
-
-
+        //rwd
+        // responsive: {
+        //     0: {
+        //         items: 1 //出現幾個
+        //     },
+        //     600: {
+        //         items: 1
+        //     },
+        //     1000: {
+        //         items: 1
+        //     }
+        // }
+    });
 }
 
+function mobileOwl() {
+    console.log("mobile ver");
+    $('.owl-carousel-one').owlCarousel({
+        loop: true, //循環
+        margin: 50, //與右邊圖片的距離
+        nav: false, //導航
+        dots: false,
+        mouseDrag: false, // 滑鼠拖曳
+        touchDrag: false, // 觸控拖曳
+        navText: ['<', '>'],
+        autoplay: true, //自動
+        startPosition: 'URLHash',
+    });
+    //最新課程
+    $('.owl-carousel-new').owlCarousel({
+        items: 1,
+        loop: true, //循環
+        margin: 10, //與右邊圖片的距離  
+        dots: false, //切換按鈕
+    });
+
+    //熱門課程輪播
+    $('.owl-carousel-best').owlCarousel({
+        items: 1,
+        loop: true, //循環
+        margin: 50, //與右邊圖片的距離
+        nav: false, //導航文字
+        dots: false,
+        autoplay: false, //自動
+    });
+
+    //理周推薦
+    $('.owl-carousel-recommend').owlCarousel({
+        items: 1,
+        loop: true, //循環
+        margin: 50, //與右邊圖片的距離
+        nav: false, //導航文字
+        dots: false,
+        autoplay: true, //自動
+    });
+
+    //影音
+    $('.owl-carousel-video').owlCarousel({
+        items: 1,
+        merge: true,
+        loop: true,
+        margin: 10,
+        video: true,
+        lazyLoad: true,
+        center: true,
+    });
+}
